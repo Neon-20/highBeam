a454d6a1655f1623dee6c5e050ace6046905a06c
Pranav Rajveer <rajveer23pr@gmail.com>
2024-07-12 19:41:23 +0530

    Update ai.ts

--- api/ai.ts
+++ api/ai.ts
@@ -39,7 +39,6 @@ export type AiModel = {
   requires_better_ai: boolean;
   context: number;
 };
-
 export async function getAvailableAiModels() {
   const res = await fetch("https://raycast.com/api/v1/ai/models");
   const models = await res.json();
